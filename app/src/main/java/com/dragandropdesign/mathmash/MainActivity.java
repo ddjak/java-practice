1043f4191baf60a2a9821a8d9bb633cd3adabcac
Djakovic <DDjakovic@CCLLABEL.CCLIND.NET>
2018-07-10 13:57:03 -0400

    some new apps

--- app/src/main/java/com/dragandropdesign/mathmash/MainActivity.java
+++ app/src/main/java/com/dragandropdesign/mathmash/MainActivity.java
@@ -1,9 +1,23 @@
 package com.dragandropdesign.mathmash;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void login(View view){
+        EditText nameField = (EditText) findViewById(R.id.editText);
+        EditText passField = (EditText) findViewById(R.id.editText2);
+
+        Toast.makeText(MainActivity.this, nameField.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        Log.i("Info", nameField.getText().toString());
+        Log.i("Info", passField.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
